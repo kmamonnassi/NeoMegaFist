215f8b51db25a9ad91fc7b5ba51dd868a8ef494b
kmamonnassi <ka82573@jk2.so-net.ne.jp>
2023-07-04 15:19:46 +0900

    ステージの壁の種類を追加

---
+++ NeoMegaFist/Assets/Scripts/Utility/ParticleSystemRotateSynchronous.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class ParticleSystemRotateSynchronous : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem particle;
+    [SerializeField] private float offset;
+
+    private void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        if (particle == null) return;
+        var main = particle.main;
+        main.startRotation = transform.localEulerAngles.z + offset;
+    }
+}
