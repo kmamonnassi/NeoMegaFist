85a6a8b92ad8f200bfc598d56f129090050f121c
kmamonnassi <ka82573@jk2.so-net.ne.jp>
2023-07-04 23:47:41 +0900

    エフェクト生成システム作成

---
+++ NeoMegaFist/Assets/Scripts/EffectPlayer/EffectPlayerInstaller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Effect
+{
+	public class EffectPlayerInstaller : MonoInstaller
+	{
+		[SerializeField] private EffectDB effectDB;
+		[SerializeField] private EffectPlayer effectPlayer;
+
+		public override void InstallBindings()
+		{
+			Container.BindInstance<IEffectDB>(effectDB);
+			Container.BindInstance<IEffectPlayer>(effectPlayer);
+		}
+	}
+}
