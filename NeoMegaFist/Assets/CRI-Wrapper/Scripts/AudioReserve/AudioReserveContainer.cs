14f5c693059c47444fc234b25008836a7f30f473
ayaha401 <oshizawadai.1192@gmail.com>
2023-07-07 15:06:19 +0900

    音予約機能のデータのKeyとValueが逆だった問題を解決。また、データを上書きする方式から以前のデータにどんどん加算していく方式に変更

--- NeoMegaFist/Assets/CRI-Wrapper/Scripts/AudioReserve/AudioReserveContainer.cs
+++ NeoMegaFist/Assets/CRI-Wrapper/Scripts/AudioReserve/AudioReserveContainer.cs
@@ -29,13 +29,48 @@ public class AudioReserveContainer
     public void SaveLog()
     {
         AudioReserveLogData logData = new AudioReserveLogData();
-        logData.speakerArray = audioReserveDic.Keys.ToArray();
-        logData.containTextArray = audioReserveDic.Values.ToArray();
+        logData.speakerArray = audioReserveDic.Values.ToArray();
+        logData.containTextArray = audioReserveDic.Keys.ToArray();
 
         string jsonStr = JsonUtility.ToJson(logData, true);
         string sceneName = SceneManager.GetActiveScene().name;
         string jsonName = AudioReserveStaticData.LOG_JSON_NAME;
         string dataPath = Application.dataPath + AudioSettingStaticData.JSON_DIRECTORY_PATH + "/AudioLogData/" + sceneName + "_" + jsonName;
+
+        // 以前のデータと見比べる
+        if (File.Exists(dataPath))
+        {
+            AudioReserveLogData beforeLogData = new AudioReserveLogData();
+            StreamReader streamReader = new StreamReader(dataPath);
+            string beforeDataStr = streamReader.ReadToEnd();
+            streamReader.Close();
+            beforeLogData = JsonUtility.FromJson<AudioReserveLogData>(beforeDataStr);
+
+            bool isNotDefauleBeforeData = beforeLogData.containTextArray.Length != 0 && beforeLogData.speakerArray.Length != 0;
+            if (isNotDefauleBeforeData)
+            {
+                HashSet<string> containTextHashSet = new HashSet<string>();
+                List<string> speakerList = new List<string>();
+                containTextHashSet = logData.containTextArray.ToHashSet();
+                speakerList = logData.speakerArray.ToList();
+
+                int beforeDataCount = beforeLogData.containTextArray.Length;
+                for (int i = 0; i < beforeDataCount; i++)
+                {
+                    if (!containTextHashSet.Contains(beforeLogData.containTextArray[i]))
+                    {
+                        containTextHashSet.Add(beforeLogData.containTextArray[i]);
+                        speakerList.Add(beforeLogData.speakerArray[i]);
+                    }
+                }
+
+                AudioReserveLogData comparedLogData = new AudioReserveLogData();
+                comparedLogData.speakerArray = speakerList.ToArray();
+                comparedLogData.containTextArray = containTextHashSet.ToArray();
+                jsonStr = JsonUtility.ToJson(comparedLogData, true);
+            }
+        }
+
         File.WriteAllText(dataPath, jsonStr);
     }
 }
