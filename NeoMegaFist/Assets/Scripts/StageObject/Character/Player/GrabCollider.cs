939036002871f284c032eb224bed9d4c16bab26c
kmamonnassi <ka82573@jk2.so-net.ne.jp>
2023-07-11 10:27:50 +0900

    Dadaboが完成 プレイヤーが上投げするときに壁に埋まらないように プレイヤーがつかむとき壁越しで掴めないように

--- NeoMegaFist/Assets/Scripts/StageObject/Character/Player/GrabCollider.cs
+++ NeoMegaFist/Assets/Scripts/StageObject/Character/Player/GrabCollider.cs
@@ -7,6 +7,7 @@ namespace StageObject
 {
     public class GrabCollider : MonoBehaviour
     {
+        [SerializeField] float rayOffset;
         public event Action<StageObjectBase> OnCatch;
 
         private void OnCollisionEnter2D(Collision2D col)
@@ -27,19 +28,29 @@ namespace StageObject
             //衝突したコライダがステージオブジェクトであり、サイズが掴めるサイズで掴むことのできるオブジェクト限定で、
             if (stageObject != null && stageObject.Size.IsCatchable() && catchAndThrow.IsCatchableObject && (catchAndThrow.State == ThrownState.Throw || catchAndThrow.State == ThrownState.Freedom))
             {
-                //このステージオブジェクトがキャラクターを継承していて、スタンしていれば掴むことができる
-                CharacterBase character = stageObject.GetComponent<CharacterBase>();
-                if(character != null)
+                Vector2 dir = ((Vector2)(stageObject.transform.position - transform.position)).normalized;
+                Vector2 origin = (Vector3)(dir * rayOffset) + transform.position;
+                float dist = Vector2.Distance(transform.position, transform.position);
+                int mask = LayerMask.GetMask("Wall", "LowWall");
+                Debug.DrawRay(origin, dir * dist, Color.green, 1);
+                var hit = Physics2D.Raycast(origin, dir, dist, mask);
+
+                if (!hit)
                 {
-                    if(character.IsStun)
+                    //このステージオブジェクトがキャラクターを継承していて、スタンしていれば掴むことができる
+                    CharacterBase character = stageObject.GetComponent<CharacterBase>();
+                    if (character != null)
                     {
+                        if (character.IsStun)
+                        {
+                            OnCatch?.Invoke(stageObject);
+                        }
+                    }
+                    else
+                    {
+                        //キャラクターを継承していなければそのまま掴める
                         OnCatch?.Invoke(stageObject);
                     }
-                }
-                else
-                {
-                    //キャラクターを継承していなければそのまま掴める
-                    OnCatch?.Invoke(stageObject);
                 }
             }
         }
